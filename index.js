5fad249c88b331a4e7bf118b9e0a77b5dfdb44cc
lucasdozie <lucasatwon@gmail.com>
2020-05-13 14:39:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.post('/', (req, res) => {
+  res.json(req.body);
+});
+
+app.listen(3000, () => console.log('server started'));
