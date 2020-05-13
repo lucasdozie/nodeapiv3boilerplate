1b4adf522212bf3f0483ff07d99769180115ad75
lucasdozie <lucasatwon@gmail.com>
2020-05-13 16:09:21 +0100

    Installing dependencies and setting up conifg

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+"use strict";
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require("fs")
+const morgan = require("morgan")
 const cors = require('cors')
-
 
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors())
+
+// create a write stream (in append mode)
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+ 
+// setup the logger
+app.use(morgan('combined', { stream: accessLogStream }))
+
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
   res.json(req.body);
